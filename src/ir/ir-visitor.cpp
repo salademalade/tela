c553e5432539868415fdb2c6244099db2500aa9e
Woojin Son <woojin.son@web.de>
2023-11-01 07:10:05 +0100

    Add IR generation of constant integers and floats

--- src/ir/ir-visitor.cpp
+++ src/ir/ir-visitor.cpp
@@ -11,20 +11,22 @@ llvm::Value *IRVisitor::visit(ASTNode *node)
 {
   switch (node->type)
   {
-    case NodeType::N_INT:
-    case NodeType::N_FLOAT:
-    case NodeType::N_ID:
-    case NodeType::N_ADD:
-    case NodeType::N_SUB:
-    case NodeType::N_MUL:
-    case NodeType::N_DIV:
-    case NodeType::N_ASSIGN:
-    case NodeType::N_FUNC_DEF:
-    case NodeType::N_FUNC_CALL:
-    case NodeType::N_RET:
-    case NodeType::N_STMT_SEQ:
-    case NodeType::N_NULL:
-    default:
-      return nullptr;
+  case NodeType::N_INT:
+    return llvm::ConstantInt::get(*context, llvm::APInt(32, std::stoi(static_cast<LeafASTNode *>(node)->value)));
+  case NodeType::N_FLOAT:
+    return llvm::ConstantFP::get(*context, llvm::APFloat(std::stof(static_cast<LeafASTNode *>(node)->value)));
+  case NodeType::N_ID:
+  case NodeType::N_ADD:
+  case NodeType::N_SUB:
+  case NodeType::N_MUL:
+  case NodeType::N_DIV:
+  case NodeType::N_ASSIGN:
+  case NodeType::N_FUNC_DEF:
+  case NodeType::N_FUNC_CALL:
+  case NodeType::N_RET:
+  case NodeType::N_STMT_SEQ:
+  case NodeType::N_NULL:
+  default:
+    return nullptr;
   }
 }
--- src/ir/ir-visitor.hpp
+++ src/ir/ir-visitor.hpp
@@ -1,9 +1,12 @@
 #ifndef IR_VISITOR_HPP
 #define IR_VISITOR_HPP
 
+#include <llvm/ADT/APInt.h>
+#include <llvm/ADT/APFloat.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Constants.h>
 #include <llvm/IR/Value.h>
 #include <memory>
 #include <string>
