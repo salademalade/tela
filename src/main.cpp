cbaa1f600d5fe6bc009bc6608d4906ed8755eb0a
Woojin Son <woojin.son@web.de>
2023-11-01 06:55:41 +0100

    Add inputting file to compiler

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 #include <cstdio>
-#include <cstdlib>
 #include "error/error.hpp"
+#include "token/token.hpp"
+#include "lexer/lexer.hpp"
+#include "ast-node/ast-node.hpp"
+#include "parser/parser.hpp"
+#include "ir/ir-visitor.hpp"
 
 int main(int argc, char **argv)
 {
   try
   {
-    fprintf(stdout, "Hello world!\n");
+    if (argc < 2) throw Error("Invalid usage.");
+
+    std::string source;
+    std::ifstream file;
+
+    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+      std::stringstream stream;
+
+      file.open(argv[1]);
+      stream << file.rdbuf();
+      file.close();
+
+      source = stream.str();
+    }
+    catch(std::ifstream::failure e)
+    {
+      throw Error("Error while reading file %s: %s", argv[1], e.what());
+    }
+
+    Lexer lexer(source);
+    std::vector<Token> toks = lexer.tokenize();
+
+    Parser parser(toks);
+    ASTNode *node = parser.parse();
+
+    IRVisitor ir_visitor(argv[1]);
+    ir_visitor.visit(node);
+
+    ir_visitor.module->print(llvm::errs(), nullptr);
   }
   catch (Error e)
   {
-    fprintf(stderr, "%s", e.what());
+    std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
 
