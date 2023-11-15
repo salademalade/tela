4310fe53e45048d3a7479ab2f631995feb3b5c1c
Woojin Son <woojin.son@web.de>
2023-11-15 17:39:08 +0100

    Add space

--- src/main.cpp
+++ src/main.cpp
@@ -52,11 +52,11 @@ int main(int argc, char **argv)
   {
     if (e.row && e.col)
     {
-      std::cerr << argv[1] << ":" << e.row << ":" << e.col << ":";
+      std::cerr << argv[1] << ":" << e.row << ":" << e.col << ": ";
     }
     else
     {
-      std::cerr << argv[0] << ":";
+      std::cerr << argv[0] << ": ";
     }
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
