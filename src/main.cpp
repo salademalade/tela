32970415812172a1e838549e4c34d5cae221cd4b
Woojin Son <woojin.son@web.de>
2023-12-05 15:50:11 +0100

    Change IRVisitor class to Module class

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,5 @@
-#include <llvm/Target/TargetOptions.h>
-#include <llvm/Target/TargetMachine.h>
-#include <llvm/Support/Host.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/raw_ostream.h>
-#include <llvm/Support/FileSystem.h>
-#include <llvm/Support/CodeGen.h>
-#include <llvm/MC/TargetRegistry.h>
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/ADT/Optional.h>
-#include <optional>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <cstdio>
 #include "error/error.hpp"
-#include "token/token.hpp"
-#include "lexer/lexer.hpp"
-#include "ast-node/ast-node.hpp"
-#include "parser/parser.hpp"
-#include "ir/ir-visitor.hpp"
+#include "module/module.hpp"
 
 int main(int argc, char **argv)
 {
@@ -28,86 +7,11 @@ int main(int argc, char **argv)
   {
     if (argc < 2) throw Error("Invalid usage.");
 
-    std::string source;
-    std::ifstream file;
+    Module module(argv[1]);
+    module.visit(module.input);
 
-    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-
-    try
-    {
-      std::stringstream stream;
-
-      file.open(argv[1]);
-      stream << file.rdbuf();
-      file.close();
-
-      source = stream.str();
-    }
-    catch(std::ifstream::failure e)
-    {
-      throw Error("Error while reading file %s: %s", argv[1], e.what());
-    }
-
-    Lexer lexer(source);
-    std::vector<Token> toks = lexer.tokenize();
-
-    Parser parser(toks);
-    ASTNode *node = parser.parse();
-
-    IRVisitor ir_visitor(argv[1]);
-    ir_visitor.visit(node);
-
-    const char *sfile_name = ".temp.ll";
-
-    std::error_code s_e_code;
-    llvm::raw_fd_ostream irdest(sfile_name, s_e_code, llvm::sys::fs::OpenFlags::OF_None);
-
-    if (s_e_code) throw Error("Could not open file: %s", s_e_code.message().c_str());
-
-    ir_visitor.module->print(irdest, nullptr);
-    irdest.flush();
-
-    auto target_triple = llvm::sys::getDefaultTargetTriple();
-
-    llvm::InitializeAllTargetInfos();
-    llvm::InitializeAllTargets();
-    llvm::InitializeAllTargetMCs();
-    llvm::InitializeAllAsmParsers();
-    llvm::InitializeAllAsmPrinters();
-
-    std::string err;
-    auto target = llvm::TargetRegistry::lookupTarget(target_triple, err);
-
-    if (!target) throw Error("%s", err.c_str());
-
-    auto CPU = "generic";
-    auto features = "";
-
-    llvm::TargetOptions opt;
-    auto RM = llvm::Optional<llvm::Reloc::Model>();
-    auto target_machine = target->createTargetMachine(target_triple, CPU, features, opt, RM);
-
-    ir_visitor.module->setDataLayout(target_machine->createDataLayout());
-    ir_visitor.module->setTargetTriple(target_triple);
-
-    char *ofile_name = argv[1];
-    strcat(ofile_name, ".o");
-
-    std::error_code o_e_code;
-    llvm::raw_fd_ostream dest(ofile_name, o_e_code, llvm::sys::fs::OpenFlags::OF_None);
-
-    if (o_e_code) throw Error("Could not open file: %s", o_e_code.message().c_str());
-
-    llvm::legacy::PassManager pass;
-    auto filetype = llvm::CodeGenFileType::CGFT_ObjectFile;
-
-    if (target_machine->addPassesToEmitFile(pass, dest, nullptr, filetype))
-    {
-      throw Error("Could not emit object file.");
-    }
-
-    pass.run(*ir_visitor.module);
-    dest.flush();
+    module.gen_ll();
+    module.gen_obj();
   }
   catch (Error e)
   {
--- src/module/module.hpp
+++ src/module/module.hpp
@@ -1,8 +1,18 @@
-#ifndef IR_VISITOR_HPP
-#define IR_VISITOR_HPP
+#ifndef MODULE_HPP
+#define MODULE_HPP
 
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Support/Host.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/CodeGen.h>
+#include <llvm/MC/TargetRegistry.h>
+#include <llvm/ADT/Optional.h>
 #include <llvm/ADT/APInt.h>
 #include <llvm/ADT/APFloat.h>
+#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
@@ -12,12 +22,19 @@
 #include <llvm/IR/Value.h>
 #include <llvm/IR/Type.h>
 #include <llvm/IR/Verifier.h>
+#include <optional>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstdio>
 #include <utility>
 #include <memory>
 #include <string>
-#include <stack>
 #include <map>
 #include "error/error.hpp"
+#include "token/token.hpp"
+#include "lexer/lexer.hpp"
 #include "ast-node/ast-node.hpp"
 #include "ast-node/leaf/leaf-ast-node.hpp"
 #include "ast-node/binary/binary-ast-node.hpp"
@@ -26,18 +43,25 @@
 #include "ast-node/func-call/func-call-ast-node.hpp"
 #include "ast-node/stmt-seq/stmt-seq-ast-node.hpp"
 #include "ast-node/null/null-ast-node.hpp"
+#include "parser/parser.hpp"
 
-class IRVisitor
+class Module
 {
 public:
+  ASTNode *input;
+  std::string filename;
+
   std::unique_ptr<llvm::LLVMContext> context;
   std::unique_ptr<llvm::IRBuilder<>> builder;
-  std::unique_ptr<llvm::Module> module;
+  std::unique_ptr<llvm::Module> llvm_module;
   std::map<std::string, llvm::AllocaInst *> sym_table;
 
-  IRVisitor(std::string mod_name);
+  Module(std::string mod_name);
 
   llvm::Value *visit(ASTNode *node);
+
+  void gen_ll();
+  void gen_obj();
 private:
   llvm::Value *visit_identifier(LeafASTNode *node);
   llvm::Value *visit_binary(BinaryASTNode *node);
