1c81bfecfb878b805da3d9c130c3374cc3a387a3
Woojin Son <woojin.son@web.de>
2023-12-14 20:53:12 +0100

    Change existing STL stuff

--- std/io.c
+++ std/io.c
@@ -1,12 +1,6 @@
-#ifdef _WIN32
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif
-
 #include <stdio.h>
 
-DLLEXPORT void print(char *str)
+void print(char *str)
 {
   fprintf(stdout, "%s", str);
 }
